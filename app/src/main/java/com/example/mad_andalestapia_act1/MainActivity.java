7d5b1131a9b07993e2d9ca867b1d1c804ed1e338
Neil112719 <phantomantonneil@gmail.com>
2023-09-19 10:46:51 +0800

    adding the files

---
+++ app/src/main/java/com/example/mad_andalestapia_act1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mad_andalestapia_act1;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private Button increase;
+    private Button decrease;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        increase = findViewById(R.id.increase);
+        decrease = findViewById(R.id.decrease);
+
+        increase.setOnClickListener(view -> {
+            float currentSize = text.getTextSize();
+            float newSize=currentSize + 2;
+            text.setTextSize(newSize);
+        });
+        decrease.setOnClickListener(view -> {
+            float currentSize=text.getTextSize();
+            float newSize= currentSize - 2;
+            text.setTextSize(newSize);
+        });
+    }
+}
