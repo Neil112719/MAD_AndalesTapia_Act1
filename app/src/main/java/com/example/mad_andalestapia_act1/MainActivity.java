c7d32f4e01a560d04c1f8de218f1f5247f46229a
Neil112719 <phantomantonneil@gmail.com>
2023-09-19 13:42:24 +0800

    Mana aestitik nalang kulang

--- app/src/main/java/com/example/mad_andalestapia_act1/MainActivity.java
+++ app/src/main/java/com/example/mad_andalestapia_act1/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.mad_andalestapia_act1;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
     private Button increase;
     private Button decrease;
-
+    private Button color;
+    private int currentColorIndex=0;
+    private int[] colorArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,17 +20,25 @@ public class MainActivity extends AppCompatActivity {
         text = findViewById(R.id.text);
         increase = findViewById(R.id.increase);
         decrease = findViewById(R.id.decrease);
+        color = findViewById(R.id.color);
+        colorArray = new int[]{
+                ContextCompat.getColor(this,R.color.purple_200),
+                ContextCompat.getColor(this,R.color.purple_500),
+                ContextCompat.getColor(this,R.color.teal_200),
+        };
 
         decrease.setOnClickListener(view -> {
-            float currentSize=text.getTextSize();
-            float newSize= currentSize - 2;
-            text.setTextSize(newSize);
+            text.setTextSize(20);
         });
         increase.setOnClickListener(view -> {
-            float currentSize = text.getTextSize();
-            float newSize=currentSize + 2;
-            text.setTextSize(newSize);
+            text.setTextSize(40);
         });
-
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentColorIndex = (currentColorIndex + 1) % colorArray.length;
+                text.setTextColor(colorArray[currentColorIndex]);
+            }
+        });
     }
 }
