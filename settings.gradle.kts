4678746ed6d4019a80faaa3d72f76f758045987d
Neil112719 <phantomantonneil@gmail.com>
2023-09-19 09:10:15 +0800

    adding the files

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_AndalesTapia_Act1"
+include(":app")
+ 
